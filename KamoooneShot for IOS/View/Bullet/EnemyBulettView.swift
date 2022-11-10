8497574b25a5649a6bf8a7677204152dd975aabb
k.kondo <k.kondo@saver.jp>
2022-11-10 17:46:48 +0900

    ３点弾処理リファクタリング。

--- KamoooneShot for IOS/View/Bullet/EnemyBulettView.swift
+++ KamoooneShot for IOS/View/Bullet/EnemyBulettView.swift
@@ -24,103 +24,111 @@ class EnemyBulletView: BaseBulletView {
     }
     
     func Update(_x: CGFloat, _y: CGFloat){
-        
         switch nowBulletType {
         case bulletType.normalBullet.rawValue:
-            // 弾発射前処理
-            for i in 0..<ZIKIMAXBULLET {
-                if !isBulletTrigger[i] && bulletStartTime > bulletDuration {
-                    isBulletTrigger[i] = true
-                    body[i].size = CGSize(width: 10, height: 10)
-                    body[i].position = CGPoint(x: _x, y: _y)
-                    GameManager.shared.scene?.addChild(body[i])
-                    bulletStartTime = 0
-                }
-            }
-            
-            // 弾移動処理
-            for i in 0..<ZIKIMAXBULLET {
-                if isBulletTrigger[i] {
-                    // 弾発射処理
-                    body[i].position.y -= 2
-                    body[i].run(SKAction.moveTo(y: body[i].position.y, duration: 0))
-                    
-                    if body[i].position.y <= (GameManager.shared.scene?.frame.minY)! || body[i].position.y >= (GameManager.shared.scene?.frame.maxY)! ||
-                        body[i].position.x <= (GameManager.shared.scene?.frame.minX)! || body[i].position.x >= (GameManager.shared.scene?.frame.maxX)! {
-                        isBulletTrigger[i] = false
-                        body[i].removeFromParent()
-                    }
-                    
-                }
-                // 弾発射インタバル
-                else if bulletStartTime <= bulletDuration {
-                    bulletStartTime += 1
-                }
-            }
+            NormalBullet(__x: _x, __y: _y)
             break
-            
         case bulletType.tripleBullet.rawValue:
-            // 弾の種類によって弾の間隔時間を変更する
-            bulletDuration = 50
-            // 弾発射前処理
-            for i in 0..<ZIKIMAXBULLET {
-                if !isBulletTrigger[i] && bulletStartTime > bulletDuration {
-                    isBulletTrigger[i] = true
-                    body[i].size = CGSize(width: 10, height: 10)
-                    body[i].position = CGPoint(x: _x, y: _y)
-                    GameManager.shared.scene?.addChild(body[i])
-                    bulletStartTime = 0
-                    
-                    // ターゲット方向のベクトルを求める
-                    let vecX = RIGHTVECTOR_X
-                    let vecY = VECTOR_Y
-                    
-                    // 三平方の定理を使って長さを求める
-                    let length:Float = sqrt(vecX + vecY)
-                    
-                    // ベクトルを求めた長さで割り、正規化にする
-                    if tripleBulletNo != 1 {
-                        directionX.append(vecX / length)
-                    } else {
-                        directionX.append((vecX / length) * -1)
-                    }
-                    directionY.append(vecY / length)
-                    tripleBulletNo += 1
-                    
-                    if tripleBulletNo == 3 {
-                        tripleBulletNo = 0
-                    }
-                }
-            }
-            
-            // 弾移動処理
-            for i in 0..<ZIKIMAXBULLET {
-                if isBulletTrigger[i] {
-                    if tripleBulletNo != 0 {
-                        body[i].position.x += CGFloat(directionX[i] * BULLET_SPEED)
-                        body[i].run(SKAction.moveTo(x: body[i].position.x, duration: 0))
-                    }
-                    body[i].position.y -= CGFloat(directionY[i] * BULLET_SPEED)
-                    body[i].run(SKAction.moveTo(y: body[i].position.y, duration: 0))
-                    
-                    if body[i].position.y <= (GameManager.shared.scene?.frame.minY)! || body[i].position.y >= (GameManager.shared.scene?.frame.maxY)! ||
-                        body[i].position.x <= (GameManager.shared.scene?.frame.minX)! || body[i].position.x >= (GameManager.shared.scene?.frame.maxX)! {
-                        isBulletTrigger[i] = false
-                        body[i].removeFromParent()
-                    }
-                    tripleBulletNo += 1
-                    if tripleBulletNo == 3 {
-                        tripleBulletNo = 0
-                    }
-                }
-                // 弾発射インタバル
-                else if bulletStartTime <= bulletDuration {
-                    bulletStartTime += 1
-                }
-            }
+            TripleBullet(__x: _x, __y: _y)
             break
         default:
             break
+        }
+    }
+    
+    func NormalBullet(__x: CGFloat, __y: CGFloat) {
+        // 弾の種類によって弾の間隔時間を変更する
+        bulletDuration = 50
+        // 弾発射前処理
+        for i in 0..<ZIKIMAXBULLET {
+            if !isBulletTrigger[i] && bulletStartTime > bulletDuration {
+                isBulletTrigger[i] = true
+                body[i].size = CGSize(width: 10, height: 10)
+                body[i].position = CGPoint(x: __x, y: __y)
+                GameManager.shared.scene?.addChild(body[i])
+                bulletStartTime = 0
+            }
+        }
+        // 弾移動処理
+        for i in 0..<ZIKIMAXBULLET {
+            if isBulletTrigger[i] {
+                // 弾発射処理
+                body[i].position.y -= 2
+                body[i].run(SKAction.moveTo(y: body[i].position.y, duration: 0))
+                
+                // 画面エリア外判定
+                if body[i].position.y <= (GameManager.shared.scene?.frame.minY)! || body[i].position.y >= (GameManager.shared.scene?.frame.maxY)! ||
+                    body[i].position.x <= (GameManager.shared.scene?.frame.minX)! || body[i].position.x >= (GameManager.shared.scene?.frame.maxX)! {
+                    isBulletTrigger[i] = false
+                    body[i].removeFromParent()
+                }
+                
+            }
+            // 弾発射インタバル
+            else if bulletStartTime <= bulletDuration {
+                bulletStartTime += 1
+            }
+        }
+    }
+    
+    func TripleBullet(__x: CGFloat, __y: CGFloat) {
+        // 弾の種類によって弾の間隔時間を変更する
+        bulletDuration = 50
+        // 弾発射前処理
+        for i in 0..<ZIKIMAXBULLET {
+            if !isBulletTrigger[i] && bulletStartTime > bulletDuration {
+                isBulletTrigger[i] = true
+                body[i].size = CGSize(width: 10, height: 10)
+                body[i].position = CGPoint(x: __x, y: __y)
+                GameManager.shared.scene?.addChild(body[i])
+                bulletStartTime = 0
+                
+                // ターゲット方向のベクトルを求める
+                let vecX = RIGHTVECTOR_X
+                let vecY = VECTOR_Y
+                
+                // 三平方の定理を使って長さを求める
+                let length:Float = sqrt(vecX + vecY)
+                
+                // ベクトルを求めた長さで割り、正規化にする
+                if tripleBulletNo != 1 {
+                    directionX.append(vecX / length)
+                } else {
+                    directionX.append((vecX / length) * -1)
+                }
+                directionY.append(vecY / length)
+                tripleBulletNo += 1
+                
+                if tripleBulletNo == 3 {
+                    tripleBulletNo = 0
+                }
+            }
+        }
+        // 弾移動処理
+        for i in 0..<ZIKIMAXBULLET {
+            if isBulletTrigger[i] {
+                if tripleBulletNo != 0 {
+                    body[i].position.x += CGFloat(directionX[i] * BULLET_SPEED)
+                    body[i].run(SKAction.moveTo(x: body[i].position.x, duration: 0))
+                }
+                body[i].position.y -= CGFloat(directionY[i] * BULLET_SPEED)
+                body[i].run(SKAction.moveTo(y: body[i].position.y, duration: 0))
+                
+                // 画面エリア外判定
+                if body[i].position.y <= (GameManager.shared.scene?.frame.minY)! || body[i].position.y >= (GameManager.shared.scene?.frame.maxY)! ||
+                    body[i].position.x <= (GameManager.shared.scene?.frame.minX)! || body[i].position.x >= (GameManager.shared.scene?.frame.maxX)! {
+                    isBulletTrigger[i] = false
+                    body[i].removeFromParent()
+                }
+                tripleBulletNo += 1
+                if tripleBulletNo == 3 {
+                    tripleBulletNo = 0
+                }
+            }
+            // 弾発射インタバル
+            else if bulletStartTime <= bulletDuration {
+                bulletStartTime += 1
+            }
         }
     }
 }
