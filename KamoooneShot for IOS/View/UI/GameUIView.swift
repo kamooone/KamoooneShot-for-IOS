bf6184e9afb826e915a424a3fb820f675ac33857
Kazusa Kondo <hahaha.kazusa@gmail.com>
2023-01-09 15:47:23 +0900

    弾と弾の衝突時の爆発の位置の修正とスコア表示

--- KamoooneShot for IOS/View/UI/GameUIView.swift
+++ KamoooneShot for IOS/View/UI/GameUIView.swift
@@ -10,8 +10,9 @@ import SpriteKit
 
 class GameUIView {
     private static var isSingleton: Bool = false
-    private var _tapText: SKLabelNode?
-    private var _title: SKSpriteNode?
+    private var tapText: SKLabelNode?
+    private var title: SKSpriteNode?
+    private var scoreText: SKLabelNode?
     private var unHideAction : SKAction!
     
     init(){
@@ -20,18 +21,28 @@ class GameUIView {
             // Nodeを非表示させるアクションを作る.
             unHideAction = SKAction.hide()
             // テキストのノードを作成する。
-            _tapText = SKLabelNode(fontNamed: "American Typewriter Bold")
-            _tapText!.text = "タップしてスタート！"
-            _tapText!.fontSize = 20
+            tapText = SKLabelNode(fontNamed: "American Typewriter Bold")
+            tapText!.text = "タップしてスタート！"
+            tapText!.fontSize = 20
             // 表示位置を画面中央
-            _tapText!.position = CGPoint(x: (GameManager.shared.scene?.frame.midX)!, y: (GameManager.shared.scene?.frame.midY)!);
-            GameManager.shared.scene?.addChild(_tapText!)
+            tapText!.position = CGPoint(x: (GameManager.shared.scene?.frame.midX)!, y: (GameManager.shared.scene?.frame.midY)!);
+            GameManager.shared.scene?.addChild(tapText!)
+            
+            // テキストのノードを作成する。
+            scoreText = SKLabelNode(fontNamed: "American Typewriter Bold")
+            scoreText!.text = "Score"
+            scoreText!.fontSize = 20
+            // 表示位置を画面中央
+            scoreText!.position = CGPoint(x: (GameManager.shared.scene?.frame.minX)! + 50, y: (GameManager.shared.scene?.frame.maxY)! - 120);
+            GameManager.shared.scene?.addChild(scoreText!)
+            
             GameUIView.isSingleton = true
         }
     }
     
     func Update(){
         // タップしてスタートの文字を削除
-        _tapText!.run(unHideAction)
+        tapText!.run(unHideAction)
+        scoreText!.text = "Score"+"100"
     }
 }
